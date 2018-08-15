f345413043b02d787a492a1c485bd259ad46673e
GetSwastik <Getswastik@gmail.com>
2018-08-15 17:28:39 +0530

    admin page update

--- resources/views/members/profile.blade.php
+++ resources/views/members/profile.blade.php
@@ -13,28 +13,31 @@
 
         <div class="column is-4 left-sidebar">
 
-          
-          <!-- left sidebar top ends -->
-          <div class="left-sidebar-bottom">
-            <ul>
-              <li>
-                <span><i class="fa fa-map-marker fa-lg"></i> From:</span>
-                <strong class="is-pulled-right">{{$user->city}}</strong>
-              </li>
-              <li>
-                <span><i class="fa fa-globe"></i> Country:</span>
-                <strong class="is-pulled-right">{{$user->country->name}}</strong>
-              </li>
-              <li>
-                <span><i class="fa fa-database"></i> Total Post:</span>
-                <strong class="is-pulled-right">{{$post}}</strong>
-              </li>
-              <li>
-                <span><i class="fa fa-heart"></i> Followers:</span>
-                <strong class="is-pulled-right">{{$follower}}</strong>
-              </li>
-            </ul>
+          <div class="left-sidebar-top">
+            <img src="/avatar/{{$user->avatar}}">
+            <h1>{{$user->name}}</h1>
+            <h2>{{$user->city}}, {{$user->country->name}}</h2>
+
+            <div class="column">
+              <ul>
+                @if(isset ($user->behance))
+                    <li><a href="{{$user->behance}}" target="_blank"><img src="{{asset('images/icons/behance.png')}}"></a></li>
+                @endif
+                @if(isset ($user->twitter))
+                    <li><a href="{{$user->twitter}}" target="_blank"><img src="{{asset('images/icons/twitter.png')}}"></a></li>
+                @endif
+                @if(isset ($user->dribbble))
+                    <li><a href="{{$user->dribbble}}" target="_blank"><img src="{{asset('images/icons/dribble.png')}}"></a></li>
+                @endif
+              </ul>
+            </div>
+
+            <div class="edit-btn">
+              <a href="{{route('profiles.edit', $user->username)}}" class="button">Edit profile</a>
+            </div>
           </div>
+          <!-- left sidebar top ends -->
+          
 
         </div>
         <!-- end is-4 -->
