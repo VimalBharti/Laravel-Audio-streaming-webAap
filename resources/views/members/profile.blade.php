84c0e3c006dcc670ca7885c14ca38a3ffcb6cb92
GetSwastik <Getswastik@gmail.com>
2018-08-15 17:27:48 +0530

    admin page update

--- resources/views/members/profile.blade.php
+++ resources/views/members/profile.blade.php
@@ -4,7 +4,7 @@
 
   <div class="plain-footer-container public-profile-page">
     <div class="menu-bar">
-      
+      @include('_partials.navbar')
     </div>
 
     <div class="container is-widescreen with-plain-footer">
@@ -13,29 +13,7 @@
 
         <div class="column is-4 left-sidebar">
 
-          <div class="left-sidebar-top">
-            <img src="/avatar/{{$user->avatar}}">
-            <h1>{{$user->name}}</h1>
-            <h2>{{$user->city}}, {{$user->country->name}}</h2>
-
-            <div class="column">
-              <ul>
-                @if(isset ($user->behance))
-                    <li><a href="{{$user->behance}}" target="_blank"><img src="{{asset('images/icons/behance.png')}}"></a></li>
-                @endif
-                @if(isset ($user->twitter))
-                    <li><a href="{{$user->twitter}}" target="_blank"><img src="{{asset('images/icons/twitter.png')}}"></a></li>
-                @endif
-                @if(isset ($user->dribbble))
-                    <li><a href="{{$user->dribbble}}" target="_blank"><img src="{{asset('images/icons/dribble.png')}}"></a></li>
-                @endif
-              </ul>
-            </div>
-
-            <div class="edit-btn">
-              <a href="{{route('profiles.edit', $user->username)}}" class="button">Edit profile</a>
-            </div>
-          </div>
+          
           <!-- left sidebar top ends -->
           <div class="left-sidebar-bottom">
             <ul>
