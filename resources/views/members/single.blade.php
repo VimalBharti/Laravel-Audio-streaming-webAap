77eb8da79f33eb7bedface607fe0c2ba6ac80f5a
GetSwastik <Getswastik@gmail.com>
2018-08-20 23:28:06 +0530

    changes

--- resources/views/members/single.blade.php
+++ resources/views/members/single.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
   <div class="menu-bar">
-
+    @include('_partials.navbar')
   </div>
 
   <div class="single-design-page">
@@ -16,7 +16,38 @@
       </div>
 
       <section>
-        
+        <b-tabs type="is-boxed" position="is-centered" size="is-medium" expanded>
+            <b-tab-item label="CODE" icon="fas fa fa-code">
+<pre>
+<code class="language-html" data-lang="html">
+{{$single->markup}}
+</code>
+</pre>
+            </b-tab-item>
+
+            <b-tab-item label="CSS" icon="fas fa fa-css3">
+<pre>
+<code class="language-html" data-lang="css">
+{{$single->css}}
+</code>
+</pre>
+            </b-tab-item>
+
+            <b-tab-item label="PSD" icon="fas fa fa-picture-o">
+              @if(isset($single->psd))
+                  <div class="download-psd has-text-centered">
+                      <h2 class="down-ps">Download PSD file</h2>
+                      <p>Free for personel use</p>
+                      <a href="{{ URL::to('/uploads/psd/' . $single->psd) }}" download="{{$single->psd}}">
+                          <img src="{{asset('images/psd-down.png')}}">
+                      </a>
+                  </div>
+              @else
+                  <h3 class="not-avail">PSD not available!</h3>
+              @endif
+            </b-tab-item>
+
+        </b-tabs>
       </section>
 
       <div class="content posted-by-single-page">
@@ -32,10 +63,9 @@
 
     </div>
     <!-- container ends -->
+
   </div>
 
-  @include('pages.join-com')
+<Myplainfooter></Myplainfooter>
 
-  @include('_partials.footer')
-
-@stop
+@endsection
