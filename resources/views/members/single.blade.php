f7192c0db694909b3c6a42a9d4264f55c8c21799
GetSwastik <Getswastik@gmail.com>
2018-08-20 18:53:16 +0530

    major changes

--- resources/views/members/single.blade.php
+++ resources/views/members/single.blade.php
@@ -68,13 +68,3 @@
 <Myplainfooter></Myplainfooter>
 
 @endsection
-@extends('scripts')
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123989535-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-123989535-1');
-</script>
-@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,15 @@
 let mix = require('laravel-mix');
+var webpack = require('webpack')
+
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
 
 /*
  |--------------------------------------------------------------------------
