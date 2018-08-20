584c1ea96e085954bc459c79d6bc445d134ff9a2
GetSwastik <Getswastik@gmail.com>
2018-08-20 19:48:44 +0530

    changes

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -29,4 +30,21 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    public function showLinkRequestForm()
+    {
+        $this->data['title'] = trans('backpack::base.reset_password'); // set the page title
+        return view('backpack::auth.passwords.email', $this->data);
+    }
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+        $passwords = config('backpack.base.passwords', config('auth.defaults.passwords'));
+        return Password::broker($passwords);
+    }
+
 }
