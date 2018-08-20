971e70145e75a33acdbf582b5f92364acd0921b1
GetSwastik <Getswastik@gmail.com>
2018-08-20 19:26:10 +0530

    changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,15 +3,10 @@
 require('./bootstrap');
 
 import Vue from 'vue'
-import VueAnalytics from 'vue-analytics'
 
 import Buefy from 'buefy'
 Vue.use(Buefy);
 
-Vue.use(VueAnalytics, {
-  id: 'UA-123989535-1',
-  checkDuplicatedScript: true
-})
 
 let userdashboardSidebar = require('./components/userdashboardSidebar.vue');
 let Preloader = require('./components/Preloader.vue');
