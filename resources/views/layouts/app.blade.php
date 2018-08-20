6710aab74aee044c99fd27e28e619584a6dc769a
GetSwastik <Getswastik@gmail.com>
2018-08-20 23:34:05 +0530

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,10 +29,11 @@
     </div>
 
 
-    <script src="{{ asset('js/app.js') }}"></script>
+
     <script src="{{ asset('js/master.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123989535-1"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
 
 </body>
