5e7da2aa91d0a8453d83561bb431363743f1a07d
GetSwastik <Getswastik@gmail.com>
2018-08-20 23:14:51 +0530

    changes

--- resources/views/members/single.blade.php
+++ resources/views/members/single.blade.php
@@ -10,7 +10,6 @@
 
   <div class="single-design-page">
 
-
     <div class="container">
       <div class="featured-image-single has-text-centered">
         <img src="/uploads/design/{{ $single->image }}" class="is-fullheight is-fullwidth">
@@ -55,14 +54,19 @@
         <p><i class="fa fa-edit"></i> Submitted By:
           <strong><a href="/profile/{{$single->user_slug}}">{{$single->user_name}}</a></strong>
         </p>
-      
+        @if(isset($single->credit))
+        <p><i class="fa fa-edit"></i> Design By:
+          <strong><a href="{{$single->url}}" target="_blank">{{$single->credit}}</a></strong>
+        </p>
+        @endif
       </div>
 
     </div>
     <!-- container ends -->
-
   </div>
 
-<Myplainfooter></Myplainfooter>
+  @include('pages.join-com')
 
-@endsection
+  @include('_partials.footer')
+
+@stop
