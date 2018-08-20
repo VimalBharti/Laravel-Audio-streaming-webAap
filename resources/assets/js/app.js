70c12770e872d25e131a122d4dffe04b83f50a4e
GetSwastik <Getswastik@gmail.com>
2018-08-20 19:11:25 +0530

    major changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,11 +7,6 @@ window.Vue = require('vue');
 import Buefy from 'buefy'
 Vue.use(Buefy);
 
-import VueAnalytics from 'vue-analytics'
-Vue.use(VueAnalytics, {
-  id: 'UA-123989535-1'
-});
-
 let userdashboardSidebar = require('./components/userdashboardSidebar.vue');
 let Preloader = require('./components/Preloader.vue');
 
