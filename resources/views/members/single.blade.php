7451a049b3bca1e4fd9e80b383e9de819907fb9d
GetSwastik <Getswastik@gmail.com>
2018-08-21 12:54:20 +0530

    changes

--- resources/views/members/single.blade.php
+++ resources/views/members/single.blade.php
@@ -40,10 +40,18 @@
         </div>
         <div id="tab-content">
           <div class="is-active tab-item" data-content="1">
-
+<pre>
+<code>
+{{$single->markup}}
+</code>
+</pre>
           </div>
           <div data-content="2" class="tab-item">
-
+<pre>
+<code>
+{{$single->css}}
+</code>
+</pre>
           </div>
           <div data-content="3" class="tab-item">
             @if(isset($single->psd))
