407aa456c9dcc96abc75bb855937c2ad45b3c08e
GetSwastik <Getswastik@gmail.com>
2018-08-23 07:56:34 +0530

    changes

--- database/migrations/2018_08_21_180028_create_post_tag_table.php
+++ database/migrations/2018_08_21_180028_create_post_tag_table.php
@@ -16,9 +16,9 @@ class CreatePostTagTable extends Migration
         Schema::create('post_tag', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('post_id')->unsigned();
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->integer('tag_id')->unsigned();
-            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
 
